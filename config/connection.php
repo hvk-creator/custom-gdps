3410b65a92208a5bed431f913b22a30225aa779c
hvk-creator <hvk261208@proton.me>
2026-09-16 19:35:26 +0700

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,23 @@
 <?php
-$servername = "mariadb-navy-slope-ps.cloud.layerbase.dev";
-$port = 12725;
-$username = "root";
-$password = "8g4MIBMcXmStMUjsZynoosUo";
-$dbname = "mariadb";
+$servername = "serverless-europe-west3.sysp0000.db2.skysql.com";
+$port = 4060;
+$username = "dbpgf27378294";
+$password = "DaZXV7AIox2.fIJ7tk9vuYYD3";
+$dbname = "gdps";
+
+$db = mysqli_init();
+
+// Set a strict connection timeout so PHP fails fast instead of hanging Railway
+$db->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+
+// Enable SSL for SkySQL
+if (getenv('DB_SSL') === 'true' || $servername) {
+    $db->ssl_set(NULL, NULL, NULL, NULL, NULL);
+    $db->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+}
+
+if (!@$db->real_connect($servername, $username, $password, $dbname, (int)$port, NULL, MYSQLI_CLIENT_SSL)) {
+    header('HTTP/1.1 500 Internal Server Error');
+    die("SkySQL Connection Error: " . mysqli_connect_error());
+}
 ?>
